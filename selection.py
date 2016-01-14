016c243f5489477b5cea6f7c73e787fb2880e234
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2016-01-14 18:51:10 +0530

    Population scaling

--- selection.py
+++ selection.py
@@ -1,30 +1,34 @@
-#Generate Random Numbers
 from random import uniform
 import matplotlib.pyplot as plt
-from math import pow
-#Assuming we're generating 8 bit integers only
+#import numpy.array as np
+
+POPULATION_SIZE = 100
+LENGTH_OF_BIT_STRING = 32
+LOWER_LIMIT = 0
+UPPER_LIMIT = 100
+
+
 population = []
-
-limit_lower=0;
-limit_upper=100;
-
-for j in range(100):
+scaled_population =[]
+for j in range(POPULATION_SIZE):
     sample = []
-    for i in range(32):
+    for i in range(LENGTH_OF_BIT_STRING): #problem with this is that as we increase our bit string sizes, our integers will also increase in value
+                                          #Maybe this is because of a small sample set
         if(uniform(0,1)<0.5):
-           sample.append('0')#builds a list of strings
+            sample.append('0')#builds a list of strings
         else:
-           sample.append('1')
+            sample.append('1')
     stringified_sample = ''.join(sample)#converts list of strings into string
-    foo1=int(stringified_sample,2);#converts string into binary
-    foo2=(limit_lower+((limit_upper-limit_lower)/float(2**32-1)))#finding the equivalent value in range  
-    population.append(foo2*foo1)#generating initial population 
-
-print(min(population))
-print(max(population))
-
-#At this point we have generated a population of 100 random integers
+    population.append(int(stringified_sample,2))#converts string into binary and adds to our population set
+#We now have a population of POPULATION_SIZE
 print(population)
 
-plt.scatter(range(100),population)
-plt.show()
+#Scale to enter Limits
+for val in population:
+    val *= LOWER_LIMIT+((UPPER_LIMIT-LOWER_LIMIT)/float(2**LENGTH_OF_BIT_STRING-1))
+    scaled_population.append(int(val))
+print(scaled_population)
+#Now our Population should be scaled according to our limits
+#print(population)
+#plt.scatter(range(POPULATION_SIZE),scaled_population)
+#plt.show()
