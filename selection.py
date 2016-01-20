cafaf73c9c191609da2ab1a32ca3ecdd7b28e9b7
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2016-01-20 15:34:20 +0530

    boobs

--- selection.py
+++ selection.py
@@ -1,15 +1,19 @@
 from random import uniform
-import matplotlib.pyplot as plt
-#import numpy.array as np
+#import matplotlib.pyplot as plt #Only if Plotting
+import operator
+import numpy as np
 
-POPULATION_SIZE = 100
+POPULATION_SIZE = 30
 LENGTH_OF_BIT_STRING = 32
 LOWER_LIMIT = 0
 UPPER_LIMIT = 100
 
+def fitness(x):
+    #currently set to x^2, later this will be inputted by the user
+    return x*x
 
+fitness_list = []
 population = []
-scaled_population =[]
 for j in range(POPULATION_SIZE):
     sample = []
     for i in range(LENGTH_OF_BIT_STRING): #problem with this is that as we increase our bit string sizes, our integers will also increase in value
@@ -24,11 +28,35 @@ for j in range(POPULATION_SIZE):
 print(population)
 
 #Scale to enter Limits
-for val in population:
-    val *= LOWER_LIMIT+((UPPER_LIMIT-LOWER_LIMIT)/float(2**LENGTH_OF_BIT_STRING-1))
-    scaled_population.append(int(val))
-print(scaled_population)
-#Now our Population should be scaled according to our limits
+for i in range(0,POPULATION_SIZE):
+    population[i] *= LOWER_LIMIT+((UPPER_LIMIT-LOWER_LIMIT)/float(2**LENGTH_OF_BIT_STRING-1))
+    population[i] = int(population[i])
+print(population)
+
+#for val in population:
+#    val *= LOWER_LIMIT+((UPPER_LIMIT-LOWER_LIMIT)/float(2**LENGTH_OF_BIT_STRING-1))
+#    scaled_population.append(int(val))
+#    fitness_list.append(fitness(int(val)))
+
+#Here I have my population and fitness in a dictionary for easy access and reference.
+#population_and_fitness = dict(zip(scaled_population,fitness_list))
+
+#print(scaled_population)
+#print(population_and_fitness)
+
+#matingpool = []
+#        -----SELECTION--------
+#key, value = max(population_and_fitness.iteritems(), key=lambda x:x[1])
+#print(key,value)
+#matingpool.append(key)
+#del population_and_fitness[key]
+#print(population_and_fitness)
+
+
+
+#print(population_and_fitness)
+#Now our population should be scaled according to our limits
 #print(population)
 #plt.scatter(range(POPULATION_SIZE),scaled_population)
+#plt.scatter(range(POPULATION_SIZE),fitness_list)
 #plt.show()
